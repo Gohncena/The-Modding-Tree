89a0b148469f3c769fe41860d2823047ace69ebf
Gohncena <146045294+Gohncena@users.noreply.github.com>
2025-01-07 15:50:29 -0800

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -13,14 +13,16 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0.1",
-	name: "IDK",
+	num: "0.0.1.1",
+	name: "the e apocalypse",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
+       <h3>v0.0.1.1</h3><br>
+		- Added changelog!
 	<h3>v0.0.1</h3><br>
 		- Added things.<br>
-		- Added stuff.`
+		- Added everything.`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
